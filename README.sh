f9d140c07df1f27a9bbb6ab1b1ed9653d0b70033
pierregibrenne <86473959+pierregibrenne@users.noreply.github.com>
2023-09-01 10:36:15 +0200

    Create README.sh

---
+++ README.sh
@@ -0,0 +1,9 @@
+
+
+# Back office pour beatmaker qui CRUD ses musics dans BD pour que les visiteurs écoutent ça
+
+Site web ou le visiteurs peut ecouter, trier les musiques par styles, bpm, et le beatmaker lui, peut inserer ses musics dans la base de données avec des info sur le sons comme le genre ou le BPM.CRUD 
+
+PHP modéle MVC a l'ancienne 
+SQL PHPmyAdmin
+
